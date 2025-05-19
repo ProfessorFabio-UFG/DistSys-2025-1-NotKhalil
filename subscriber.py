3ff9b3b739372b9b3d996c17dd4e66ef96f64484
Khalil <khalilmotta@outlook.com>
2025-05-18 23:04:23 -0300

    Update subscriber.py

--- subscriber.py
+++ subscriber.py
@@ -1,12 +1,15 @@
 import zmq
-from constPS import * #-
+from constPS import *  #-
 
 context = zmq.Context()
 s = context.socket(zmq.SUB)          # create a subscriber socket
-p = "tcp://"+ HOST +":"+ PORT        # how and where to communicate
+p = "tcp://" + HOST + ":" + PORT     # how and where to communicate
 s.connect(p)                         # connect to the server
-s.setsockopt_string(zmq.SUBSCRIBE, "TIME")  # subscribe to TIME messages
+# Subscribe to all topics
+s.setsockopt_string(zmq.SUBSCRIBE, "TIME")
+s.setsockopt_string(zmq.SUBSCRIBE, "Weather")
+s.setsockopt_string(zmq.SUBSCRIBE, "humidity")
 
-for i in range(5):  # Five iterations
-	time = s.recv()   # receive a message
-	print (bytes.decode(time))
+for _ in range(5):  # Five iterations
+    message = s.recv()               # receive a message
+    print(bytes.decode(message))
