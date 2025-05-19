42573e1ac2d7eaad25c19537cb3c9e852f86ae32
Khalil <khalilmotta@outlook.com>
2025-05-18 23:04:11 -0300

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -1,11 +1,21 @@
 import zmq, time
-from constPS import * #-
+from constPS import *  #-
 
 context = zmq.Context()
 s = context.socket(zmq.PUB)        # create a publisher socket
-p = "tcp://"+HOST+":"+ PORT      # how and where to communicate
+p = "tcp://" + HOST + ":" + PORT  # how and where to communicate
 s.bind(p)                          # bind socket to the address
+
 while True:
-	time.sleep(5)                    # wait every 5 seconds
-	msg = str.encode("TIME " + time.asctime())
-	s.send(msg) # publish the current time
+    time.sleep(5)                   # wait every 5 seconds
+    # Publish TIME
+    msg_time = str.encode("TIME " + time.asctime())
+    s.send(msg_time)
+    # Publish Weather
+    weather_status = "Sunny in Goiânia, Goiás, Brazil"
+    msg_weather = str.encode(f"Weather {weather_status}")
+    s.send(msg_weather)
+    # Publish humidity
+    humidity_value = "65% in Goiânia, Goiás, Brazil"
+    msg_humidity = str.encode(f"humidity {humidity_value}")
+    s.send(msg_humidity)
